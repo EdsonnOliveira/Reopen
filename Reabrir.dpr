2ecf1333b1506ee0d74ffeb9e6489e5a2aed45ed
EdsonnOliveira <72781591+EdsonnOliveira@users.noreply.github.com>
2021-12-10 14:55:41 -0300

    Add files via upload

---
+++ Reabrir.dpr
@@ -0,0 +1,14 @@
+program Reabrir;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'Forms\uPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
